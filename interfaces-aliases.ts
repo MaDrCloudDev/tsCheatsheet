627c19685dcef1ae9e791ca219460b9984a51f7f
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-05-20 21:20:58 -0700

    add interfaces-aliases section
    add interfaces-aliases section

---
+++ interfaces-aliases.ts
@@ -0,0 +1,47 @@
+//
+
+// type aliases
+type VehicleYear = number;
+type VehicleMaker = string;
+type VehicleModel = string;
+type Vehicle = {
+	year: VehicleYear;
+	maker: VehicleMaker;
+	model: VehicleModel;
+};
+
+const vehicleYear: VehicleYear = 2008;
+const vehicleMaker: VehicleMaker = 'Honda';
+const vehicleModel: VehicleModel = 'Civic';
+const vehicle: Vehicle = {
+	year: vehicleYear,
+	maker: vehicleMaker,
+	model: vehicleModel,
+};
+
+// interfaces
+interface Rectangle {
+	height: number;
+	width: number;
+}
+
+const rectangle: Rectangle = {
+	height: 22,
+	width: 11,
+};
+
+// extending interfaces
+interface Rectangle {
+	height: number;
+	width: number;
+}
+
+interface ColorRectangle extends Rectangle {
+	color: string;
+}
+
+const colorRectangle: ColorRectangle = {
+	height: 22,
+	width: 11,
+	color: 'green',
+};
