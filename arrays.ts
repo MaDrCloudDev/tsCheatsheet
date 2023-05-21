2c7575048744b836c04b1471feda6643e1a6330e
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-05-20 19:04:53 -0700

    finish arrays section
    finish arrays section

---
+++ arrays.ts
@@ -0,0 +1,14 @@
+// Arrays
+
+const names: string[] = [];
+names.push('Matt'); // no error
+// names.push(3); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+
+// Readonly
+const names: readonly string[] = ['Matt'];
+names.push('Michael'); // Error: Property 'push' does not exist on type 'readonly string[]'.
+
+// Type inference
+const numbers = [1, 2, 3]; // inferred to type number[]
+numbers.push(4); // no error
+numbers.push('4'); // Error: Argument of type 's
