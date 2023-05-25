5d28f67aecb059a649cda9ff3aa2bf225b5efa40
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-05-24 17:49:58 -0700

    Update arrays.ts

--- arrays.ts
+++ arrays.ts
@@ -11,4 +11,5 @@ names.push('Michael'); // Error: Property 'push' does not exist on type 'readonl
 // Type inference
 const numbers = [1, 2, 3]; // inferred to type number[]
 numbers.push(4); // no error
-numbers.push('4'); // Error: Argument of type 's
+numbers.push('4'); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+let head: number = numbers[0]; // no error
