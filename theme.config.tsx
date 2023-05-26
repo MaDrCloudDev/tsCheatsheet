13459a3af4b840300519e39d7b1a074c6d5b74ec
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-05-26 14:45:02 -0700

    updates, context, disclaimers, logo
    updates, context, disclaimers, logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
 	logo: <span>typeScriptCheatSheet</span>,
 	project: {
 		link: 'https://github.com/madrclouddev/typeScriptCheatSheet',
+		icon: <img src='/madrlogo.svg' />,
 	},
-	// chat: {
-	// 	link: 'https://discord.com',
-	// },
+	head: () => {
+		const { asPath, defaultLocale, locale } = useRouter();
+		const { frontMatter } = useConfig();
+		const url =
+			'https://my-app.com' +
+			(defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+		const currentUrl = url.replace('https://my-app.com/', '');
+		console.log(currentUrl);
+		const typeScriptCheatsheet = 'typeScriptCheatSheet';
+		return (
+			<>
+				<title>
+					{currentUrl || typeScriptCheatsheet} | TSCT | MaDrCloudDev
+				</title>
+				<meta
+					property='og:description'
+					content={frontMatter.description || 'MaDrCloudDev TypeScript'}
+				/>
+			</>
+		);
+	},
 	docsRepositoryBase: 'https://github.com/madrclouddev',
 	footer: {
-		text: 'typeScriptCheatSheet',
+		text: (
+			<span>
+				MaDrCloudDev {new Date().getFullYear()}{' '}
+				<a
+					href='https://github.com/madrclouddev/typeScriptCheatSheet'
+					target='_blank'>
+					typeScriptCheatSheet
+				</a>
+				.
+			</span>
+		),
 	},
 };
 
