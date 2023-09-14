5d490edc9557946a5d5aa472cdc574846b7ffba5
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-09-14 13:14:15 -0700

    sitemap, search not working in production
    sitemap, search not working in production

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://tscheatsheet.madr.io/',
 	integrations: [
 		starlight({
 			title: 'tsCheatSheet',
