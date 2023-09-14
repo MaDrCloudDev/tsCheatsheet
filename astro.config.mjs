f74fd869d34f89dfa26638707a7bcc984a1a0059
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-09-14 13:39:36 -0700

    add cloudflare adapter
    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,70 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tscheatsheet.madr.io/',
-	integrations: [
-		starlight({
-			title: 'tsCheatSheet',
-			social: {
-				github: 'https://github.com/MaDrCloudDev/tsCheatsheet',
-			},
-			sidebar: [
-				{
-					label: 'TypeScript',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Arrays', link: '/typescript/arrays' },
-						{ label: 'Casting', link: '/typescript/casting' },
-						{ label: 'Classes', link: '/typescript/classes' },
-						{ label: 'Enums', link: '/typescript/enums' },
-						{ label: 'Functions', link: '/typescript/functions' },
-						{ label: 'Generics', link: '/typescript/generics' },
-						{
-							label: 'Interfaces & Aliases',
-							link: '/typescript/interfaces_and_aliases',
-						},
-						{ label: 'Keyof', link: '/typescript/keyof' },
-						{ label: 'Null', link: '/typescript/null_and_undefined' },
-						{ label: 'Object Types', link: '/typescript/object_types' },
-						{ label: 'Tuples', link: '/typescript/tuples' },
-						{ label: 'Types', link: '/typescript/types' },
-						{ label: 'Union Types', link: '/typescript/union_types' },
-						{ label: 'Utility Types', link: '/typescript/utility_types' },
-					],
-				},
-				// {
-				// 	label: 'Reference',
-				// 	autogenerate: { directory: 'reference' },
-				// },
-			],
-		}),
-	],
+  site: 'https://tscheatsheet.madr.io/',
+  integrations: [starlight({
+    title: 'tsCheatSheet',
+    social: {
+      github: 'https://github.com/MaDrCloudDev/tsCheatsheet'
+    },
+    sidebar: [{
+      label: 'TypeScript',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Arrays',
+        link: '/typescript/arrays'
+      }, {
+        label: 'Casting',
+        link: '/typescript/casting'
+      }, {
+        label: 'Classes',
+        link: '/typescript/classes'
+      }, {
+        label: 'Enums',
+        link: '/typescript/enums'
+      }, {
+        label: 'Functions',
+        link: '/typescript/functions'
+      }, {
+        label: 'Generics',
+        link: '/typescript/generics'
+      }, {
+        label: 'Interfaces & Aliases',
+        link: '/typescript/interfaces_and_aliases'
+      }, {
+        label: 'Keyof',
+        link: '/typescript/keyof'
+      }, {
+        label: 'Null',
+        link: '/typescript/null_and_undefined'
+      }, {
+        label: 'Object Types',
+        link: '/typescript/object_types'
+      }, {
+        label: 'Tuples',
+        link: '/typescript/tuples'
+      }, {
+        label: 'Types',
+        link: '/typescript/types'
+      }, {
+        label: 'Union Types',
+        link: '/typescript/union_types'
+      }, {
+        label: 'Utility Types',
+        link: '/typescript/utility_types'
+      }]
+    }
+    // {
+    // 	label: 'Reference',
+    // 	autogenerate: { directory: 'reference' },
+    // },
+    ]
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
